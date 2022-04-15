5c8677f44ae8000b285edd77a4db79b27587e822
JaeHyun <kei03151@naver.com>
2022-04-15 14:01:30 +0900

    22.04.15

---
+++ src/test/java/com/mega/book/springboot/web/dto/HelloResponseDtoTest.java
@@ -0,0 +1,20 @@
+package com.mega.book.springboot.web.dto;
+
+import com.mega.book.web.dto.HelloResponseDto;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HelloResponseDtoTest {
+
+    @Test
+    public void loombookTest(){
+        String name = "test";
+        int amount = 1000;
+
+        HelloResponseDto dto = new HelloResponseDto(name, amount);
+
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+    }
+}
